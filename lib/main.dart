6d22ea3f1f30826ef562a2c3caa5a15338730071
jardianoalmeida <61389908+jardianoalmeida@users.noreply.github.com>
2020-04-05 15:08:58 -0300

    Adicionando o mapa

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flare_splash_screen/flare_splash_screen.dart';
+/*import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:teste/splash/splash_screen.dart';
 
@@ -19,6 +19,46 @@ class MyApp extends StatelessWidget {
         routes: {
           '/index': (context) => IndexPage(),
         },
+    );
+  }
+}*/
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -6,6 +6,7 @@ import 'package:flare_flutter/flare_actor.dart';
 import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:volume/volume.dart';
 
 import '../main.dart';
 
@@ -15,13 +16,32 @@ class MSplashScreen extends StatefulWidget {
 }
 
 class _MSplashScreenState extends State<MSplashScreen> {
+
   var foto =
       "iVBORw0KGgoAAAANSUhEUgAAAtAAAAXoCAYAAACU2fzSAAAAAXNSR0IArs4c6QAAAARzQklUCAgICHwIZIgAACAASURBVHic7N15fCRXfe/9b0vV0imNNJJmn/GMZ8H72MbYxhjwgs1uQwJctoQkxCRAtkvIDUtIuCRkIXmS5wnkhlxiCMkDhEASboCEgINjYmMwGOMN4wXj3WOP7VmkmdFMHalL6vtHbaeqqyWdsWb/vF8vmFF3V9Wp6pbnW6d/55yGLn9/WwAAAADmpedQNwAAAAA4khCgAQAAAA8EaAAAAMADARoAAADwQIAGAAAAPBCgAQAAAA8EaAAAAMADARoAAADwQIAGAAAAPBCgAQAAAA8EaAAAAMADARoAAADwQIAGAAAAPBCgAQAAAA8EaAAAAMADARoAAADwQIAGAAAAPASHugEAAADAbD607BG9b/Sx+ud2Hqff2XH8QW0PARoAAOAIFfT2aJHp08gio2WLB9TfLEe7yVas7bv3addeqwk7pXh65qC2b6C/qXXLh7VyZFDf/OFDB/XYBxIBGgAAYAE1Gg0N9Dd18ekbdMv9j+vJ8b1qt9sKent06rrlWj06pMlWrMd27NaWHbtlp2Kv/fcFvdq0aolWjQ5qKOyf8/VrlizO/74nmtQTYxN64Imdmoqnvc9tvobCfh2/fFjHrxhR0LP/FcOjPbHO7N+nF4S7JUnvW3yZ7uxdKUnaHD+hP97zNb1gYLcu3rdbt00t0q7p3gVp/1wOaIBuBr160VnP0DknrNHwQL/2Tbb0o8e262vf/7HGJiLv/b307BN0y/1btW3X3gVr41DYrxed9Yz851sfeFwPPTk+r22XDy/Ss088TksXD+ieR7fpph/Xf7UAAACOHc3eHl24eb3+5Odfond+4qvatushTbfbeuEzN+mSMzdpkelTb09D+yZb+vw379APHnxiXmF20PRp06olWrtssXr3M5QOhf0aCvu1adWotmzfrQefHNOeaHK/9lVnZJHR8ctHtH7FyNP";
 
   @override
   void initState() {
     assetsAudioPlayer.open("file/audio.mp3");
+    initPlatformState();
     super.initState();
+  }
+
+  double _sliderValue = 0.0;
+  int maxVol, currentVol;
+
+  Future<void> initPlatformState() async {
+    await Volume.controlVolume(AudioManager.STREAM_MUSIC); // you can change which volume you want to change.
+  }
+
+  updateVolumes() async {
+    maxVol = await Volume.getMaxVol;
+    currentVol = await Volume.getVol;
+    setState(() {});
+  }
+
+  setVol(int i) async {
+    await Volume.setVol(i);
   }
 
   final assetsAudioPlayer = AssetsAudioPlayer();
@@ -39,7 +59,8 @@ class _MSplashScreenState extends State<MSplashScreen> {
       // Android
       statusBarIconBrightness: Brightness.dark,
     ));
-    final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+    final GlobalKey<ScaffoldState> _scaffoldKey =
+        new GlobalKey<ScaffoldState>();
 
     return Scaffold(
       key: _scaffoldKey,
@@ -89,10 +110,23 @@ class _MSplashScreenState extends State<MSplashScreen> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    avatar(function: ()=> Navigator.pushNamed(context, "/index")), //Julio
-                    avatar(function: ()=> Navigator.pushNamed(context, "/index")), //Julio
-                    avatar(function: ()=> Navigator.pushNamed(context, "/index")), //Julio
-                    avatar(function: ()=> Navigator.pushNamed(context, "/index")), //Julio
+                    avatar(
+                        function: () => Navigator.pushNamed(context, "/index"),
+                      imagem: "file/moises.jpg"
+                    ),
+                    avatar(
+                        function: () => Navigator.pushNamed(context, "/index"),
+                        imagem: "file/julio.jpg"
+                    ),
+                    //Julio
+                    avatar(
+                        function: () => Navigator.pushNamed(context, "/index"),
+                        imagem: "file/marcos.jpg"
+                    ),
+                    avatar(
+                        function: () => Navigator.pushNamed(context, "/index"),
+                        imagem: "file/henrique.jpg"
+                    ),
                   ],
                 ),
                 AvatarGlow(
@@ -102,10 +136,12 @@ class _MSplashScreenState extends State<MSplashScreen> {
                   child: InkWell(
                     onTap: () {
                       _scaffoldKey.currentState.showSnackBar(new SnackBar(
-                        duration: Duration(seconds: 50),
-                          content: new Text("Programar também é uma arte!", textAlign: TextAlign.center,)
-                      ));
-                    },// assetsAudioPlayer.play(),
+                          duration: Duration(seconds: 50),
+                          content: new Text(
+                            "Programar também é uma arte!",
+                            textAlign: TextAlign.center,
+                          )));
+                    }, // assetsAudioPlayer.play(),
                     child: Material(
                       elevation: 8.0,
                       shape: CircleBorder(),
@@ -127,11 +163,45 @@ class _MSplashScreenState extends State<MSplashScreen> {
                 Container(
                   height: 20.0,
                   width: 200.0,
-                  child: FlareActor("file/audio.flr",
-                      alignment: Alignment.center,
-                      fit: BoxFit.contain,
-                      animation: "record",
+                  child: FlareActor(
+                    "file/audio.flr",
+                    alignment: Alignment.center,
+                    fit: BoxFit.contain,
+                    animation: "record",
                   ),
+                ),
+/*                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: () {
+                        Volume.volDown(); // Consecutively decrease the volume by 1 unit.
+                        updateVolumes();
+                      },
+                    ),
+                    IconButton(
+                      icon: Text("+", style: TextStyle(color: Colors.white70, fontSize: 40),),
+                      onPressed: () {
+                        Volume.volUp(); // Consecutively decrease the volume by 1 unit.
+                        updateVolumes();
+                      },
+                    ),
+                  ],
+                ),*/
+                Slider(
+                  activeColor: Colors.indigoAccent,
+                  min: 0.0,
+                  max: 15.0,
+                  onChanged: (newRating) async {
+                    setState(() {
+                      _sliderValue = newRating;
+                    });
+                    await setVol(newRating.toInt());
+                    await updateVolumes();
+                  },
+                  value: _sliderValue,
                 ),
                 //                 Center(child: Text("Desenvolvedor de Software", style: TextStyle(fontSize: 14.0, color: Colors.blue), textAlign: TextAlign.center,)),
               ],
@@ -142,13 +212,14 @@ class _MSplashScreenState extends State<MSplashScreen> {
     );
   }
 }
-avatar({Function function}){
+
+avatar({Function function, String imagem}) {
   return AvatarGlow(
     endRadius: 50.0,
     glowColor: Colors.white,
     showTwoGlows: true,
     child: InkWell(
-      onTap: function,// assetsAudioPlayer.play(),
+      onTap: function, // assetsAudioPlayer.play(),
       child: Material(
         elevation: 8.0,
         shape: CircleBorder(),
@@ -158,7 +229,7 @@ avatar({Function function}){
           decoration: new BoxDecoration(
             shape: BoxShape.circle,
             image: new DecorationImage(
-              image: AssetImage("file/photo.jpg"),
+              image: AssetImage(imagem),
               fit: BoxFit.cover,
             ),
           ),
